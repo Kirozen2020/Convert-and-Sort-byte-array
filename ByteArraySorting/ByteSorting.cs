c38da39bf2c1813fc90f13f09bbc6c2c2f38dffa
rozen <rozenberg.kim@gmail.com>
2024-02-11 16:09:00 +0200

    Included a byte sorting class.

---
+++ ByteArraySorting/ByteSorting.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+
+namespace ByteArraySorting
+{
+    internal class ByteSorting
+    {
+        int numberOfElements;
+        byte[] data;
+
+        public ByteSorting(int numberOfElements)
+        {
+            this.numberOfElements = numberOfElements;
+            this.data = new byte[numberOfElements];
+        }
+        public byte[] GetSortedArray()
+        {
+            FillArray();
+            SortdArray();
+            return this.data;
+        }
+
+        private void SortdArray()
+        {
+            for (int i = 0; i < this.data.Length-1; i++)
+            {
+                for (int j = 0; j < this.data.Length-i-1; j++)
+                {
+                    if (this.data[j] > this.data[j + 1])
+                    {
+                        byte temp = this.data[j];
+                        this.data[j] = this.data[j + 1];
+                        this.data[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        private void FillArray()
+        {
+            int input = 0;
+            for (int i = 0; i < this.numberOfElements; i++)
+            {
+                Console.Write($"Plese enter element {i+1}: ");
+                while (true)
+                {
+                    char inputChar = (char)Console.Read();
+                    if(inputChar >= '0' &&  inputChar <= '9')
+                    {
+                        input = input * 10 + (inputChar - '0');
+                    }
+                    else if (inputChar == '\n' || inputChar == '\0')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if(input > 255)
+                        {
+                            Console.Write("Invalid input, try again. ");
+                            i--;
+                        }
+                        else
+                        {
+                            this.data[i] = (byte)input;
+                        }
+                        
+                        input = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- ByteArraySorting/Program.cs
+++ ByteArraySorting/Program.cs
@@ -14,7 +14,7 @@ namespace ByteArraySorting
             {
                 int inputNumber = Console.Read();
                 //check if the entered character is a digit
-                if(inputNumber >= '0' && inputNumber <= '9')
+                if (inputNumber >= '0' && inputNumber <= '9')
                 {
                     numberOfElements = numberOfElements * 10 + (inputNumber - '0');
                 }
@@ -23,132 +23,22 @@ namespace ByteArraySorting
                     break;
                 }
             }
-            Console.Read();
-            byte[][] bytes = new byte[numberOfElements][];
-            char[][] chars = new char[numberOfElements][];
 
-            //colection data and converting to byte
-            for (int i = 0; i < numberOfElements; i++)
-            {
-                char[] element = new char[0];
-                Console.Write($"Please enter element {i + 1}: ");
-                while (true)
-                {
-                    char inputChar = (char)Console.Read();
-                    //the ascii code I have took from here
-                    // https://www.dotnetperls.com/ascii-table
-                    if (
-                        (inputChar >= ' ' && inputChar <= '^') ||
-                        (inputChar >= 'a' && inputChar <= '~'))
-                    { 
-                        element = AddCharToArray(element, inputChar);
-                    }
-                    else if(inputChar == '\n' ||  inputChar == '\0')
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        chars[i] = element;
-                        break;
-                    }
-                }
+            PrintByteArray(new ByteSorting(numberOfElements).GetSortedArray());
 
-            }
-            
-            //Converting: char -> byte
-
-            for (int i = 0;i < chars.Length;i++)
-            {
-                byte[] list = new byte[chars[i].Length];
-                for (int j = 0; j < chars[i].Length; j++)
-                {
-                    list[j] = (byte)chars[i][j];
-                }
-                bytes[i] = list;
-            }
-
-            bytes = SortByteArray(bytes);
-
-            //Converting: byte -> char
-
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                char[] list = new char[bytes[i].Length];
-                for (int j = 0; j < bytes[i].Length; j++)
-                {
-                    list[j] = (char)bytes[i][j];
-                }
-                chars[i] = list;
-            }
-            Console.WriteLine();
-            Console.Write("The sorted array is: ");
-            for (int i = 0; i < chars.Length-1; i++)
-            {
-                for (int j = 0; j < chars[i].Length; j++)
-                {
-                    Console.Write($"{chars[i][j]}");
-                }
-                Console.Write(", ");
-            }
-            for (int i = 0; i < chars[chars.Length-1].Length; i++)
-            {
-                Console.Write($"{chars[chars.Length-1][i]}");
-            }
-
-            
-            Console.WriteLine("\n\nPress any key for closing");
             Console.ReadKey();
-        }
-
-        private static byte[][] SortByteArray(byte[][] bytes)
-        {
-            byte[][] arr = bytes;
-
-            for (int i = 0; i < arr.Length-1; i++)
-            {
-                for (int j = i+1; j < arr.Length; j++)
-                {
-                    if (arr[i].Length > arr[j].Length)
-                    {
-                        byte[] temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                    else if (arr[i].Length == arr[j].Length)
-                    {
-                        for (int k = 0; k < arr[i].Length; k++)
-                        {
-                            if (arr[i][k] > arr[j][k])
-                            {
-                                byte[] temp = arr[i];
-                                arr[i] = arr[j];
-                                arr[j] = temp;
-                                break;
-                            }
-                            else if (arr[i][k] < arr[j][k])
-                            {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            return arr;
-        }
-
-        private static char[] AddCharToArray(char[] arr, char c)
-        {
-            char[] list = new char[arr.Length+1];
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                list[i] = arr[i];
-            }
-            list[arr.Length] = c;
-
-            return list;
+            
         }
         
+        private static void PrintByteArray(byte[] bytes)
+        {
+            Console.Write("\nSorted array: ");
+            for (int i = 0; i < bytes.Length-1; i++)
+            {
+                Console.Write(bytes[i]+", ");
+            }
+            Console.Write(bytes[bytes.Length-1]);
+        }
+
     }
 }
